c0c640d843d90a7810609a999e2d1638dfcf10e9
Osheen Gulati <osheen.gulati@nagarro.com>
2023-08-03 11:29:37 -0400

    Updating keycloak port

--- ng-keycloak/src/environments/keycloak.config.ts
+++ ng-keycloak/src/environments/keycloak.config.ts
@@ -5,7 +5,7 @@
 import { KeycloakConfig } from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8083',
   realm: 'master1',
   clientId: 'claims-ui-local',
 };
