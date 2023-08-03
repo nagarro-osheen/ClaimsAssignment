b043d425a98a398ec215891b55953749fd39ce47
Osheen Gulati <osheen.gulati@nagarro.com>
2023-08-03 11:29:59 -0400

    Post call for payment initiate

--- claims/src/main/java/com/ycompany/claims/controller/CreateClaimController.java
+++ claims/src/main/java/com/ycompany/claims/controller/CreateClaimController.java
@@ -2,20 +2,22 @@ package com.ycompany.claims.controller;
 
 
 import com.ycompany.claims.dto.ClaimApproveDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ycompany.claims.dto.ClaimRequestDto;
 import com.ycompany.claims.dto.SurveyClaimRequestDto;
 import com.ycompany.claims.entity.ClaimEntity;
 import com.ycompany.claims.enums.ClaimStatusEnum;
 import com.ycompany.claims.service.ClaimService;
+import org.apache.tomcat.util.json.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -30,10 +32,9 @@ public class CreateClaimController {
     @Autowired
     private RestTemplate restTemplate;
 
-
-
     @Autowired ClaimService claimService;
 
+    private static final Logger LOG = LoggerFactory.getLogger(CreateClaimController.class);
 
     @PostMapping("/create")
     public ClaimEntity createClaim(@RequestBody ClaimRequestDto claimRequest){
@@ -68,9 +69,21 @@ public class CreateClaimController {
     @PostMapping("/approve/{taskId}")
     public boolean approveClaim(@PathVariable String taskId){
         this.kafkaTemplate.send("user_approve_task", taskId);
+        try{
+        this.initiatePaymentRelease(taskId);}catch(JsonProcessingException e){
+            LOG.error("Exception in JSON Processing");
+        }
         return true;
     }
 
-
-
+    private void initiatePaymentRelease(String taskId) throws JsonProcessingException {
+     String uri = "http://localhost:8080/rest/GOV.UK+Pay+API/1.0.3/v1/payments";
+     RestTemplate restTemplate = new RestTemplate();
+     ResponseEntity<String> response = restTemplate.postForEntity(uri,taskId,String.class);
+     String responseJson = response.getBody();
+     ObjectMapper mapper = new ObjectMapper();
+     JsonNode node = mapper.readTree(responseJson);
+     String email = node.get("email").asText();
+     this.kafkaTemplate.send("claim_payment_initiated", email);
+     }
 }
